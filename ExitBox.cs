21461f2acd470827b2172f0c9f965153caafb5cf
snow <lookforalex920376@gmail.com>
2018-05-21 18:51:45 -0400

    add more class for different levels of the game

---
+++ ExitBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitBox : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("in collision");
+        if(col.gameObject.tag == "Ball" ||
+            col.gameObject.tag == "Red" ||
+            col.gameObject.tag == "Blue" ||
+            col.gameObject.tag == "Yellow" ||
+            col.gameObject.tag == "Green" )
+        {
+            //Debug.Log("is touch");
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ Unlock.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unlock : MonoBehaviour {
+
+    public string type;
+    public GameObject[] locked;
+    public GameObject itself;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("in collision");
+        if (col.gameObject.tag == "Yellow" ||
+            col.gameObject.tag == "Red" ||
+            col.gameObject.tag == "Green" ||
+            col.gameObject.tag == "Blue" ||
+            col.gameObject.tag == "Ball")
+        {
+            for(int i = 0; i < locked.Length; i++)
+            {
+                Destroy(locked[i]);
+            }
+            //Debug.Log("is touch");
+            Destroy(itself);
+        }
+    }
+}
