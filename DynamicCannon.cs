5741dee4d874b7d98bd56286890e452d73684958
snow <lookforalex920376@gmail.com>
2018-05-25 13:17:04 -0400

    complete level 14

--- DynamicCannon.cs
+++ DynamicCannon.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class DynamicCannon : MonoBehaviour {
 
     public List<GameObject> bulletList;
-    
+    public string[] StartFromRight = {"R", "U", "L", "D"};
+    public string[] StartFromLeft = { "L", "D", "R", "U"};
+    public string[] StartFromUp = {"U", "L", "D", "R"};
+    public string[] StartFromDown = { "D", "R", "U", "L"};
+    public string[] cannonType;
+    public GameObject border;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    // Use this for initialization
+    void Start () {
+        setStartFromList();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        changeBulletDirection();
+
+    }
 
 
     public void addToBulletList(GameObject b)
@@ -23,30 +30,83 @@ public class DynamicCannon : MonoBehaviour {
         bulletList.Add(b);
     }
 
-    void changeBulletDirection()
+
+    void setStartFromList()
     {
-        if ((int)this.transform.rotation.eulerAngles.z == 0)
+        if ((int)this.transform.rotation.eulerAngles.z == 0 &&
+            gameObject.GetComponent<Shoot>().direction == "R")
         {
-            ;
+            cannonType = StartFromRight;
+
         }
-        else if ((int)this.transform.rotation.eulerAngles.z == 270)
+        else if((int)this.transform.rotation.eulerAngles.z == 0 &&
+            gameObject.GetComponent<Shoot>().direction == "U")
         {
-            drop_helper();
+            cannonType = StartFromUp;
         }
-        else if ((int)this.transform.rotation.eulerAngles.z == 90)
+        else if((int)this.transform.rotation.eulerAngles.z == 0 &&
+           gameObject.GetComponent<Shoot>().direction == "D")
         {
-            drop_helper();
+            cannonType = StartFromDown;
         }
-        else if ((int)this.transform.rotation.eulerAngles.z == 180)
+        else if ((int)this.transform.rotation.eulerAngles.z == 0 &&
+           gameObject.GetComponent<Shoot>().direction == "L")
         {
-            drop_helper();
+            cannonType = StartFromLeft;
         }
     }
-    void changeBulletHelper(string cannonDir, string bulletDir)
+
+    void changeBulletDirection()
     {
+        Debug.Log(border.transform.rotation.eulerAngles.z);
+        if ((int)border.transform.rotation.eulerAngles.z > -1 &&
+            (int)border.transform.rotation.eulerAngles.z < 1)
+        {
+            changeBulletHelper(cannonType[0]);
+        }
+        else if ((int)border.transform.rotation.eulerAngles.z > 89 &&
+            (int)border.transform.rotation.eulerAngles.z < 91)
+        {
+            changeBulletHelper(cannonType[1]);
+        }
+        else if ((int)border.transform.rotation.eulerAngles.z > 179 &&
+            (int)border.transform.rotation.eulerAngles.z <181)
+        {
+            changeBulletHelper(cannonType[2]);
+        }
+        else if ((int)border.transform.rotation.eulerAngles.z > 269 &&
+            (int)border.transform.rotation.eulerAngles.z <271)
+        {
+            changeBulletHelper(cannonType[3]);
+        }
+    }
+    void changeBulletHelper(string cannonDir)
+    {
+        string bulletDir;
+        if (cannonDir == "U" || cannonDir == "D")
+        {
+            bulletDir = "V";
+        }
+        else
+        {
+            bulletDir = "H";
+        }
+        int bullDis;
+        if(cannonDir == "L" || cannonDir == "D")
+        {
+            bullDis = -30;
+        }
+        else
+        {
+            bullDis = 30;
+        }
+        gameObject.GetComponent<Shoot>().direction = cannonDir;
         foreach (Transform child in transform)
         {
             //child is your child transform
+            Bullet c = child.GetComponent<Bullet>();
+            c.direction = bulletDir;
+            c.distance = bullDis;
         }
     }
 }
