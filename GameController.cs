120c666a93ba2941de508576f7790393982a8a93
snow <lookforalex920376@gmail.com>
2018-06-02 00:26:19 -0400

    add gamecotroller

---
+++ GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController GameControl;
+    public int HighestScore;
+    public int BallMaterial;
+    public int BorderMaterial;
+
+	// Use this for initialization
+	void Awake () {
+        if (GameControl == null)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(GameControl != this)
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+}
