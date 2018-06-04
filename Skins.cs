3169bffbb1aa3802a2063830ef6a59a14902c639
snow <lookforalex920376@gmail.com>
2018-06-04 01:32:02 -0400

    add more skin for the border

--- Skins.cs
+++ Skins.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 
 public class Skins : MonoBehaviour {
 
-    public  Sprite[] Zebra_Red = new Sprite[4];
+    public Sprite[] Gold;
+    public Sprite[] Green;
+    public Sprite[] Grey;
+    public Sprite[] LightBlue;
+    public Sprite[] Purple;
+    public Sprite[] Red;
+    public Sprite[] Hollow_Gold;
+    public Sprite[] Hollow_Green;
+    public Sprite[] Hollow_Grey;
+    public Sprite[] Hollow_LightBlue;
+    public Sprite[] Hollow_Purple;
+    public Sprite[] Hollow_Red;
+    public  Sprite[] Zebra_Red;
     public  Sprite[] Zebra_Blue;
     public  Sprite[] Zebra_Yellow;
     public  Sprite[] Zebra_Green;
@@ -18,7 +30,55 @@ public class Skins : MonoBehaviour {
         //gameObject.GetComponent<GameController>().HighestScore++;
         //Debug.Log(Control.BorderMaterials);
         GameController Control = GameController.GameControl;
-        if (str == "Zebra_Red")
+        if (str == "Gold")
+        {
+            Control.BorderMaterials = Gold;
+        }
+        else if (str == "Green")
+        {
+            Control.BorderMaterials = Green;
+        }
+        else if (str == "Grey")
+        {
+            Control.BorderMaterials = Grey;
+        }
+        else if (str == "LightBlue")
+        {
+            Control.BorderMaterials = LightBlue;
+        }
+        else if (str == "Purple")
+        {
+            Control.BorderMaterials = Purple;
+        }
+        else if (str == "Red")
+        {
+            Control.BorderMaterials = Red;
+        }
+        else if (str == "Hollow_Gold")
+        {
+            Control.BorderMaterials = Hollow_Gold;
+        }
+        else if (str == "Hollow_Green")
+        {
+            Control.BorderMaterials = Hollow_Green;
+        }
+        else if (str == "Hollow_Grey")
+        {
+            Control.BorderMaterials = Hollow_Grey;
+        }
+        else if (str == "Hollow_LightBlue")
+        {
+            Control.BorderMaterials = Hollow_LightBlue;
+        }
+        else if (str == "Hollow_Purple")
+        {
+            Control.BorderMaterials = Hollow_Purple;
+        }
+        else if (str == "Hollow_Red")
+        {
+            Control.BorderMaterials = Hollow_Red;
+        }
+        else if (str == "Zebra_Red")
         {
             Control.BorderMaterials = Zebra_Red;
         }
